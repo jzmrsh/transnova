30e40cf6652389cba4ca242f260b693de18da400
JӨZÜΣMΛЯƧH <53591150+jzmrsh@users.noreply.github.com>
2026-02-11 16:08:32 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/transnova/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
